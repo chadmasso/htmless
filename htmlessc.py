d5ba4e0c998fd5ba000e55f024607b69e7d4e6be
Chad Masso <chad.m.masso@gmail.com>
2011-09-21 17:19:44 -0500

    cleaned a bit

--- htmlessc.py
+++ htmlessc.py
@@ -5,16 +5,13 @@ from jinja2 import FileSystemLoader, Environment
 dest_path = '../'
 htmless_file = sys.argv[1]
 env = Environment(loader=FileSystemLoader(os.path.dirname(htmless_file)))
-template = env.get_template(os.path.basename(htmless_file))
+file_name = os.path.basename(htmless_file)
+template = env.get_template(file_name)
 result = template.render()
-dest = os.path.basename(htmless_file)[0:os.path.basename(htmless_file).index('.htmless')]
+dest = os.path.splitext(file_name)[0]
 
 destination = open(dest_path + dest + '.html', 'w')
 destination.write(result)
 destination.close()
 
 print "Compiled and Saved"
-
-         
-
-
